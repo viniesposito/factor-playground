d544e7f049257704cf456db2976db876bb6e77f2
Vinicius Esposito <viniesposito@gmail.com>
2020-10-23 10:21:52 -0400

    duh

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 ticker = 'FCNTX'
 
 data = get_whole_sample_factor_loadings(ticker)
